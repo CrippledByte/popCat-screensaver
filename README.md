c810e8a3df8c471cfb1c4b69a7a3d904842b89cd
CrippledByte <CrippledByte@users.noreply.github.com>
2023-01-23 18:37:43 +0100

    Add url parameter for speed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # popCat-screensaver
 
 [View live page](https://popcat.crippled.dev/) or download the repository if you really like popCat.
+
+## Speed
+The movement speed can be set using the `speed` url parameter (default value is `3`).
+[Example](https://popcat.crippled.dev?speed=2.5).
--- script.js
+++ script.js
@@ -46,13 +46,17 @@ var direction = 'se'
 let prev_time = new Date();
 let pop_timeout = null;
 
+// Get speed from url
+const urlParams = new URLSearchParams(window.location.search);
+const speed_original = urlParams.get('speed') || 3;
+
 function display (rect, canvas, context) {
   let curr_time = new Date();
   let time_diff = curr_time - prev_time;
   prev_time = curr_time;
 
   const speed_modifier = time_diff / (1000 / 60);
-  let speed = 3 * speed_modifier;
+  let speed = speed_original * speed_modifier;
 
   // If image hits top
   let hit = false;
